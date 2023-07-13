c5166a0f3800635e0503af07b96bb8c47773ea63
smoonknight <satriacandrasmk@gmail.com>
2023-07-13 09:57:48 +0700

    Fix some bug

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'smoonknight/genchanline',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8a012759a3632f2a0c95bea1d0056dd9ede892c0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'smoonknight/genchanline' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8a012759a3632f2a0c95bea1d0056dd9ede892c0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
