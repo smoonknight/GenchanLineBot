fb46fb6e0957364f9ee715ca874e9d4e74d451b0
smoonknight <satriacandrasmk@gmail.com>
2023-08-01 00:45:03 +0700

    Update feature scraping

--- App/ScrapingController.php
+++ App/ScrapingController.php
@@ -8,7 +8,17 @@ class ScrapingController
 {
     public static function GenshinImpactHoneyScraping($characterName)
     {
-        return $characterName;
+        $url = @"https://genshin.honeyhunterworld.com/$characterName/?lang=EN";
+        $html = file_get_html($url);
+        $characterDescription = $html->find('', 0);
+
+        if ($characterDescription)
+        {
+            $name = $characterDescription->find("tr", 0)->find("td", 1)->plaintext;
+            return $name;
+        }
+
+        return "kosong";
     }
 }
 ?>
