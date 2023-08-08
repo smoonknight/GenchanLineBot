d8d7e1c136ebe0a7fa768fbe77b7427912b468b0
smoonknight <satriacandrasmk@gmail.com>
2023-08-08 12:25:38 +0700

    Update feature scraping

--- App/ScrapingController.php
+++ App/ScrapingController.php
@@ -114,7 +114,7 @@ class ScrapingController
 
                 $increment++;
 
-                if ($content = "")
+                if ($content == "")
                 {
                     continue;
                 }
