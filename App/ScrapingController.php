1792651c5f9498b24de239238463e3a598eeb8e3
smoonknight <satriacandrasmk@gmail.com>
2023-08-08 07:32:37 +0700

    Update feature scraping

--- App/ScrapingController.php
+++ App/ScrapingController.php
@@ -13,24 +13,15 @@ class ScrapingController
 
         $characterDescription = $html->find('table[class="genshin_table main_table"]', 0);
 
-        $data = [
-            'name' => $characterDescription->find("tr", 1)->find("td", 1)->plaintext,
-            'title' => $characterDescription->find("tr", 2)->find("td", 1)->plaintext,
-            'occupation' => $characterDescription->find("tr", 3)->find("td", 1)->plaintext,
-            'rarity' => $characterDescription->find("tr", 4)->find("td", 1)->plaintext,
-            'weapon' => $characterDescription->find("tr", 5)->find("td", 1)->plaintext,
-            'element' => $characterDescription->find("tr", 6)->find("td", 1)->plaintext,
-            'dayOfBirth' => $characterDescription->find("tr", 7)->find("td", 1)->plaintext,
-            'monthOfBirth' => $characterDescription->find("tr", 8)->find("td", 1)->plaintext,
-            'visionIntroduced' => $characterDescription->find("tr", 9)->find("td", 1)->plaintext,
-            'constellationIntroduced' => $characterDescription->find("tr", 10)->find("td", 1)->plaintext,
-            'chineseSeuyu' => $characterDescription->find("tr", 11)->find("td", 1)->plaintext,
-            'japaneseSeuyu' => $characterDescription->find("tr", 12)->find("td", 1)->plaintext,
-            'englishSeuyu' => $characterDescription->find("tr", 13)->find("td", 1)->plaintext,
-            'koreanSeuyu' => $characterDescription->find("tr", 14)->find("td", 1)->plaintext,
-            'description' => $characterDescription->find("tr", 15)->find("td", 1)->plaintext,
-        ];
-        
+        $data = array();
+
+        foreach ($characterDescription->find("tr") as $td)
+        {
+            $key = $td->find("td", 0)->plaintext;
+            $value = $td->find("td", 1)->plaintext;
+            $data[$key] = $value;
+        }
+
         return json_encode($data, JSON_PRETTY_PRINT);
     }
 }
