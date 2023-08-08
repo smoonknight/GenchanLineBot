31a660e545d1ee64f5032bf15a2114091f0ede10
smoonknight <satriacandrasmk@gmail.com>
2023-08-08 14:37:21 +0700

    Update feature scraping

--- App/LineBot.php
+++ App/LineBot.php
@@ -522,7 +522,7 @@ class LineBot {
 		return $result;
 	}
 
-	public function replyAudio($audioUrl){
+	public function replyAudio($audioUrl, $duration){
 		$api = $this->apiReply;
 		$webhook = $this->webhookEventObject;
 		$replyToken = $webhook->{"events"}[0]->{"replyToken"}; 
@@ -530,6 +530,7 @@ class LineBot {
 		$body["messages"][0] =  array(
 			    'type' => 'audio',
 			    'originalContentUrl' => $audioUrl,
+			    'duration' => $duration
 			);		
 		$result = $this->httpPost($api,$body);
 		return $result;
