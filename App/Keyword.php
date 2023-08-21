acc9f20c4d8a47b720fc0aeeed24b1bce69f0460
smoonknight <satriacandrasmk@gmail.com>
2023-08-21 20:35:38 +0700

    add firewrite from memegen generator

--- App/Keyword.php
+++ App/Keyword.php
@@ -184,6 +184,25 @@ class Keyword{
         $this->bot->replyChatWithImage($result, $groupChatSummary["pictureUrl"]);
     }
 
+    public function FireWriteGenerator()
+    {
+        $parseText = $this->bot->getMessageText(true, false);
+        $linkTexted = Genchan::ArrayToText($parseText, 0, "_");
+        $linkTexted = str_replace("?", "~q", $linkTexted);
+        $linkTexted = str_replace("&", "~a", $linkTexted);
+        $linkTexted = str_replace("%", "~p", $linkTexted);
+        $linkTexted = str_replace("#", "~h", $linkTexted);
+        $linkTexted = str_replace("/", "~s", $linkTexted);
+        $linkTexted = str_replace("\\", "~b", $linkTexted);
+        $linkTexted = str_replace("<", "~l", $linkTexted);
+        $linkTexted = str_replace(">", "~g", $linkTexted);
+        $linkTexted = str_replace("''", "\"", $linkTexted);
+
+        $memegenLink = "https://api.memegen.link/images/custom/[text].gif?background=https://media.tenor.com/7UarUv_Z1QYAAAAd/gunna-fire.gif&layout=top";
+        $url = str_replace("[text]", $linkTexted, $memegenLink);
+        $this->bot->replyImage($url);
+    }
+
     public function sticker()
     {
         $firebaseController = new FirebaseController();
